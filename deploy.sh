d22a7505a4388a014d3b03539cdc3ef5bedfdf68
junilHwang <tjsdlf4261@naver.com>
2020-08-12 01:53:04 +0900

    feat: deploy 스크립트 생성

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build 후 build 폴더로 이동
+yarn build
+cd build
+
+# gh-pages의 git 이력 가져오기
+git clone -b gh-pages https://github.com/est-react-study/postman-junil
+cp -rf postman-junil/.git ./.git
+rm -rf postman-junil
+
+# 커밋
+git add .
+git commit -m "$*"
+
+# gh-pages 브랜치를 푸쉬
+git push origin gh-pages
+
+# 작업 폴더로 돌아오기
+cd -

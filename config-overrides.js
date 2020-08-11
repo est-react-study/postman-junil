63ceadca737ab75de0b7cf1064452be0f9856648
junilHwang <tjsdlf4261@naver.com>
2020-08-12 02:32:47 +0900

    feat: build:gh-pages 명령어에 대한 webpack 설정 추가

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,5 @@
 module.exports = function override(config, env) {
   if (process.env.GH_PAGES === 'true') {
-    console.log(config);
     config.output.publicPath = './'; // build 경로 변경
   }
   return config

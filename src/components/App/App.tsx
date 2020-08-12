e3abf5d77da73cdb445556332b542ce6a11aba07
junilHwang <tjsdlf4261@naver.com>
2020-08-13 01:21:37 +0900

    feat: component 구조 변경

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => (
+export const App: React.FC = () => (
   <div className="App">
     React로 만드는 Postman Clone Coding
   </div>
 );
-
-export default App;

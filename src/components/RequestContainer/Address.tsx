cd49e320cf1cee8191f3cfddf2d21e8ffbb2d9a1
junilHwang <tjsdlf4261@naver.com>
2020-08-15 15:28:26 +0900

    feat: store hook 적용

--- src/components/RequestContainer/Address.tsx
+++ src/components/RequestContainer/Address.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useRecoilState } from "recoil";
+import { methodState, methods } from "../../stores/requestMethod";
 
 
+export const Address: React.FC = () => {
 
-export const Address: React.FC = () => (
-  <header>
-    <select>
-      <option>GET</option>
-    </select>
-    <input />
-  </header>
-);
+  const [method, setMethod] = useRecoilState(methodState);
+
+  const changeMethod = ({ target }: React.ChangeEvent<HTMLSelectElement>) => {
+    setMethod(target.value);
+  }
+
+  useEffect(() => {
+    console.log(method);
+  });
+
+  return (
+    <header>
+      <select value={method} onChange={changeMethod}>
+        { methods.map((v, key) => <option value={v} key={key}>{v}</option>) }
+      </select>
+      <input />
+    </header>
+  );
+}

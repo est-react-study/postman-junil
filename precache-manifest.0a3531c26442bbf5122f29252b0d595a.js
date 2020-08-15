cfef0097c8385c6898608ac88dd9adbd62d04e98
junilHwang <tjsdlf4261@naver.com>
2020-08-15 16:10:08 +0900

    세 번째 배포

--- precache-manifest.0a3531c26442bbf5122f29252b0d595a.js
+++ precache-manifest.0a3531c26442bbf5122f29252b0d595a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "832964fe6c761f65a2733f81d5dc02d4",
+    "revision": "44027e4c7eedcb0ff6af4e64b5c3b779",
     "url": "./index.html"
   },
   {
-    "revision": "077a3c17914b65fb0f3a",
+    "revision": "72d29d8b1dfcaf819539",
     "url": "./static/css/main.cc8d0cfc.chunk.css"
   },
   {
-    "revision": "cb3d4b1268b5f56c4096",
-    "url": "./static/js/2.ed10913c.chunk.js"
+    "revision": "96a1e7eb6c1480828021",
+    "url": "./static/js/2.a8d2bd34.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "./static/js/2.ed10913c.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.a8d2bd34.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "077a3c17914b65fb0f3a",
-    "url": "./static/js/main.d5dd0f9c.chunk.js"
+    "revision": "72d29d8b1dfcaf819539",
+    "url": "./static/js/main.6dcdaf70.chunk.js"
   },
   {
     "revision": "d6f84f3bdc10b8ade6d2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a2f12df140f464d977ba1df7d5e6b978.js"
+  "./precache-manifest.0a3531c26442bbf5122f29252b0d595a.js"
 );
 
 self.addEventListener('message', (event) => {

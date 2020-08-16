beebdf7967f43f46ab3b85782baa38e7ade2031d
junilHwang <tjsdlf4261@naver.com>
2020-08-16 16:42:43 +0900

    feat: 기본 Layout UI 작업 중 (2)

--- src/components/AsideContainer/Histories.tsx
+++ src/components/AsideContainer/Histories.tsx
@@ -1,7 +1,17 @@
+/** @jsx jsx */
+import {css, jsx} from "@emotion/core";
 import React from "react";
 
+const titleStyle = css`
+  border-bottom: 2px solid #06F;
+  padding: 15px;
+  margin: 0;
+  font-size: 21px;
+  font-weight: 400;
+`;
+
 export const Histories: React.FC = () => (
-  <div>
-    histories
-  </div>
+  <section>
+    <h2 css={titleStyle}>History</h2>
+  </section>
 );
--- src/components/FormContainer/RequestContainer/RequestContainer.tsx
+++ src/components/FormContainer/RequestContainer/RequestContainer.tsx
@@ -1,9 +1,18 @@
+/** @jsx jsx **/
+import { css, jsx } from "@emotion/core";
 import React from "react";
 import { Address } from "./Address";
 
+const titleStyle = css`
+  font-size: 21px;
+  margin: 0;
+  padding: 15px;
+  font-weight: 400;
+`;
+
 export const RequestContainer: React.FC = () => (
   <section>
-    <h3>Request</h3>
-    <Address />
+    <h3 css={titleStyle}>Request</h3>
+    <Address/>
   </section>
 );

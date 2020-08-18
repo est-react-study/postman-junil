c98639b10fecce5bfb5d56107738efe3a04d4ab1
junilhwang <tjsdlf4261@naver.com>
2020-08-18 19:45:42 +0900

    feat: RequestTable의 props 작업 중 (2)

--- src/components/FormContainer/RequestContainer/RequestTable.tsx
+++ src/components/FormContainer/RequestContainer/RequestTable.tsx
@@ -1,23 +1,30 @@
 import React from "react";
+import { IRequestTable } from "stores/requestStore";
+import { SetterOrUpdater } from "recoil";
 
 interface RequestTableProps {
-  data: object[],
-  setData: React.SetStateAction<object>
+  data: IRequestTable[],
+  setData: SetterOrUpdater<IRequestTable[]>
 }
 
 export const RequestTable: React.FC<RequestTableProps> = ({ data, setData }: RequestTableProps) => {
 
   return (
     <div>
-      { data.length &&
+      { data.length !== 0 ?
         <ul>
-          { data.map((v, key) =>
-            <li key={key}>
-              {v}
+          { data.map(({ key, value }, index) =>
+            <li key={index}>
+              <input type="text" value={key} placeholder="key" />
+              <input type="text" value={value} placeholder="value" />
             </li>
           ) }
-        </ul>
+        </ul> :
+        <p>현재 등록된 파라미터가 없습니다.</p>
       }
+      <button type="button">
+        추가
+      </button>
     </div>
   )
 };

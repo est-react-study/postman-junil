8177c4bd70b000f7066213d29db7893287e8fb15
junilhwang <tjsdlf4261@naver.com>
2020-08-18 23:53:02 +0900

    feat: queryParams를 생성해주는 utils 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,13 @@
+import {IRequestTable} from "../stores/requestStore";
+
 const urlRule = /^https?:\/\/[a-zA-Z0-9가-힣._-]+\.[a-zA-Z]+$/
 export const validateURL = (url: string) => {
   return url.length > 0 && urlRule.test(url);
+}
+
+export const getQueryParamsOf = (params: IRequestTable[]) => {
+  const queryParams = params.filter(({ key, value }) => ![key.trim(), value.trim()].includes(''))
+                            .map(({ key, value }) => `${key}=${encodeURIComponent(value)}`).join('&');
+
+  return queryParams.length ? `?${queryParams}` : '';
 }

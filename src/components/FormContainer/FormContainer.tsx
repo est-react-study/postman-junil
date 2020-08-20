d935a81d1548d5429d32dbf9c1dd66989f8ec948
junilhwang <tjsdlf4261@naver.com>
2020-08-20 22:57:12 +0900

    feat: RequestContainer에 RequestAddress method 위임 중

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,19 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import {RequestContainer} from "./RequestContainer";
-import {Alert} from "components/Layout/Dialog/Alert";
 
 export const FormContainer: React.FC = () => {
-
-  const [isAlert, setAlert] = useState(true);
 
   return (
     <section className="formContainer">
       <RequestContainer />
-      { isAlert &&
-        <Alert onClose={() => setAlert(false)}>
-          test
-        </Alert>
-      }
     </section>
   )
 };
--- src/components/FormContainer/RequestContainer/RequestContainer.tsx
+++ src/components/FormContainer/RequestContainer/RequestContainer.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx **/
 import { css, jsx } from "@emotion/core";
-import React from "react";
+import React, {useState} from "react";
 import { RequestAddress } from "./RequestAddress";
 import { RequestConfig } from "./RequestConfig";
+import {Alert} from "../../Layout/Dialog";
 
 const titleStyle = css`
   font-size: 21px;
@@ -11,10 +12,20 @@ const titleStyle = css`
   font-weight: 400;
 `;
 
-export const RequestContainer: React.FC = () => (
-  <section>
-    <h3 css={titleStyle}>Request</h3>
-    <RequestAddress />
-    <RequestConfig />
-  </section>
-);
+export const RequestContainer: React.FC = () => {
+
+  const [alertMessage, setAlertMessage] = useState('');
+
+  return (
+    <section>
+      <h3 css={titleStyle}>Request</h3>
+      <RequestAddress/>
+      <RequestConfig/>
+      { alertMessage.length > 0 &&
+        <Alert onClose={() => setAlertMessage('')}>
+          { alertMessage }
+        </Alert>
+      }
+    </section>
+  );
+};

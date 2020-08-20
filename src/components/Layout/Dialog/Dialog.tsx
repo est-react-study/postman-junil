e4a02b7c3330abd437ce4469549687779698e251
junilhwang <tjsdlf4261@naver.com>
2020-08-20 18:36:14 +0900

    feat: Dialog Component 작업 중

--- src/components/Layout/Dialog/Dialog.tsx
+++ src/components/Layout/Dialog/Dialog.tsx
@@ -1,10 +1,24 @@
+/** @jsx jsx **/
+import {css, jsx} from "@emotion/core";
 import React from "react";
 
-
+const dialogStyles = css`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  background: fade-out(#000, 0.5);
+  z-index: 100;
+  
+  > div {
+    background-color: #fff;
+  }
+`;
 
 export const Dialog: React.FC = (props) => {
   return (
-    <div>
+    <div css={dialogStyles}>
       <div>
         { props.children }
       </div>

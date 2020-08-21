0abf527bd65f89fd5fa8d182f8c308cfb8233c2f
junilHwang <tjsdlf4261@naver.com>
2020-08-21 23:59:51 +0900

    feat: History 목록 띄우기 완료

--- src/components/AsideContainer/Histories.tsx
+++ src/components/AsideContainer/Histories.tsx
@@ -1,6 +1,25 @@
 /** @jsx jsx */
 import {css, jsx} from "@emotion/core";
 import React from "react";
+import {useRecoilValue} from "recoil";
+import {historyState} from "../../stores/historyStore";
+
+const historiesStyle = css`
+  ul, li {
+    list-style: none;
+  }
+  
+  ul {
+    margin: 0;
+    padding: 5px 0;
+  }
+  
+  li {
+    word-break: break-all;
+    margin: 0;
+    padding: 5px;
+  }
+`;
 
 const titleStyle = css`
   border-bottom: 2px solid #06F;
@@ -10,8 +29,27 @@ const titleStyle = css`
   font-weight: 400;
 `;
 
-export const Histories: React.FC = () => (
-  <section>
-    <h2 css={titleStyle}>History</h2>
-  </section>
-);
+const noneStyles = css`
+  text-align: center;
+  font-size: 13px;
+  color: #666;
+`;
+
+export const Histories: React.FC = () => {
+  const histories = useRecoilValue(historyState);
+  return (
+    <section css={historiesStyle}>
+      <h2 css={titleStyle}>History</h2>
+      { histories.length > 0 ?
+        <ul>
+          {histories.map((v, key) => (
+            <li key={key}>
+              {v}
+            </li>
+          ))}
+        </ul> :
+        <p css={noneStyles}>검색 내역이 없습니다.</p>
+      }
+    </section>
+  );
+}

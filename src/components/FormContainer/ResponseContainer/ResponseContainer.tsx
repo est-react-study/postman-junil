07c2a402401ccf5300a75504f6bf7e5f79b32999
junilHwang <tjsdlf4261@naver.com>
2020-08-21 23:20:37 +0900

    feat: Response 보내기전 문구 꾸미기

--- src/components/FormContainer/ResponseContainer/ResponseContainer.tsx
+++ src/components/FormContainer/ResponseContainer/ResponseContainer.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx **/
 import { jsx } from '@emotion/core';
 import React from "react";
-import { responseContainerStyles, responseTitleStyles } from "./styles";
-import {useRecoilValue} from "recoil";
-import {responseState} from "../../../stores/responseStore";
+import { responseContainerStyles, responseTitleStyles, noneBlockStyles } from "./styles";
+import { useRecoilValue } from "recoil";
+import { responseState } from "stores/responseStore";
 import Highlight from "react-highlight";
 
 export const ResponseContainer: React.FC = () => {
@@ -18,7 +18,7 @@ export const ResponseContainer: React.FC = () => {
           <Highlight>
             { JSON.stringify(response.data, null, 4) }
           </Highlight> :
-          <p>아직 Request를 보내지 않았습니다.</p>
+          <p css={noneBlockStyles}>아직 Request를 보내지 않았습니다.</p>
       }
     </section>
   );
--- src/components/FormContainer/ResponseContainer/styles.ts
+++ src/components/FormContainer/ResponseContainer/styles.ts
@@ -11,3 +11,14 @@ export const responseTitleStyles = css`
   padding: 15px;
   font-weight: 400;
 `;
+
+export const noneBlockStyles = css`
+  margin: 0 15px 15px;
+  background: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+  color: #666;
+  border-radius: 3px;
+  border: 1px solid #ddd;
+  font-size: 13px;
+`;

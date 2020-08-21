ae15758b064a2b7222a764865d866c081170eeed
junilHwang <tjsdlf4261@naver.com>
2020-08-21 22:24:38 +0900

    feat: ResponseContainer 추가

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {RequestContainer} from "./RequestContainer";
+import {ResponseContainer} from "./ResponseContainer";
 
 export const FormContainer: React.FC = () => {
 
   return (
     <section className="formContainer">
       <RequestContainer />
+      <ResponseContainer />
     </section>
   )
 };
---
+++ src/components/FormContainer/ResponseContainer/ResponseContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ResponseContainer: React.FC = () => {
+  return (
+    <section>
+      아직 Request를 보내지 않았습니다.
+    </section>
+  );
+}

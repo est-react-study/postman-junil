968376fdd4a6484d25a50ad8b48816903b6fa7b9
junilhwang <tjsdlf4261@naver.com>
2020-08-30 19:39:00 +0900

    20200830 배포 (1)

---
+++ precache-manifest.b12fda248dcfe05fa32b56bd606ca1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1466b36d4b1c262c8e34a6b5094ac4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "606727c84fa7ef6e0830",
+    "url": "./static/css/2.1cbedbbb.chunk.css"
+  },
+  {
+    "revision": "657c706b1bc85bda070c",
+    "url": "./static/css/main.5c199501.chunk.css"
+  },
+  {
+    "revision": "606727c84fa7ef6e0830",
+    "url": "./static/js/2.79927201.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.79927201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657c706b1bc85bda070c",
+    "url": "./static/js/main.a7f6c7bb.chunk.js"
+  },
+  {
+    "revision": "d6f84f3bdc10b8ade6d2",
+    "url": "./static/js/runtime-main.a48ce466.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2ffab385697197dd98f0fbf406364558.js"
+  "./precache-manifest.b12fda248dcfe05fa32b56bd606ca1ed.js"
 );
 
 self.addEventListener('message', (event) => {
